2c5a3f353f2e00a38ff83ce5ce70147319370164
kisetzuu <keithvillanueva213@gmail.com>
2024-06-28 09:36:35 +0800

    Revised Home UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,6 +35,23 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className="featured-products">
+        <h2>Featured Products</h2>
+        <div className="product-list">
+          <div className="product-item">
+            <img src={process.env.PUBLIC_URL + '/product1.jpg'} alt="Product 1" className="product-image" />
+            <p>Razer Basilisk V3</p>
+          </div>
+          <div className="product-item">
+            <img src={process.env.PUBLIC_URL + '/product2.jpg'} alt="Product 2" className="product-image" />
+            <p>GeForce RTX 4090</p>
+          </div>
+          <div className="product-item">
+            <img src={process.env.PUBLIC_URL + '/product3.jpg'} alt="Product 3" className="product-image" />
+            <p>ROG Pugio</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
