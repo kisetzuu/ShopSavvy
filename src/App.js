bae50a5a47c142cfb5c500385a1b2db35026ee6f
MarkToroy <markjohntoroy2016@gmail.com>
2024-07-02 00:03:03 +0800

    [About Page][Mark]
    Adding the About Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Home from './pages/Home';
 import CartPage from './pages/CartPage';
 import LoginPage from './pages/LoginPage';
+import AboutPage from './pages/AboutPage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the About Page where we will provide information about this website.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
