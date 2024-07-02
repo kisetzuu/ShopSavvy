dc85adfe0939c2f4e51345a1839edee34a1c448e
rhongobongo <epicjeff03@gmail.com>
2024-07-02 12:31:39 +0800

    Header for login

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
+import { auth } from '../../services/FirebaseConfig';
 
 const Header = () => {
   const [menuActive, setMenuActive] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+    } catch (error) {
+      console.error("Error logging out: ", error);
+    }
+  };
 
   const toggleMenu = () => {
     setMenuActive(!menuActive);
@@ -24,6 +46,27 @@ const Header = () => {
           <li><a href="/contact">Contact</a></li>
           <li><a href="/cart">Cart</a></li>
           <li><a href="/login">Login</a></li>
+          <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <button type="submit">Search</button>
+        </div>
+        {user ? (
+            <>
+              <li>{user.email}</li>
+              <li>
+                <button onClick={handleLogout}>Logout</button>
+              </li>
+            </>
+          ) : (
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          )}
         </ul>
         <div className="menu-toggle" onClick={toggleMenu}>
           <span className="bar"></span>
